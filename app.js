c7315855b47a989d24096ee844a53702774ed5e7
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2023-03-28 11:40:24 -0300

    finish the module

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require("path");
 
-console.log(routes.someText);
+const  app = express();
 
-const server = http.createServer(routes.handler);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const notFoundRoutes = require('./routes/not_found')
 
-server.listen(3000);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(notFoundRoutes);
+
+
+app.listen(3000);
---
+++ routes/not_found.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const rootDir = require('../utils/path')
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+});
+
+module.exports = router;
