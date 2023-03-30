ff449aec5de5cd7ee41cf3fa0941f8381b7f49e7
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2023-03-30 11:21:26 -0300

    dynamic content

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const path = require("path");
 
 const  app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views')
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const notFoundRoutes = require('./routes/not_found')
-
+const notFoundRoutes = require('./routes/not_found');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 app.use(notFoundRoutes);
+
 
 
 app.listen(3000);
--- routes/not_found.js
+++ routes/not_found.js
@@ -6,7 +6,7 @@ const rootDir = require('../utils/path')
 const router = express.Router();
 
 router.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.render('404')
 });
 
 module.exports = router;
